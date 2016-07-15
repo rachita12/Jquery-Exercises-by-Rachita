514e76b8b43b652ad59adcd2b39bf1b530f991c5
rachita12 <rachitanayyar23@gmail.com>
2016-07-15 13:27:31 +0530

    Jquery exercise 5.1

---
+++ jquery-exercise5.1/js/blog.js
@@ -0,0 +1,27 @@
+function Blog(blogId, textId){
+	"use strict"
+	this.blogId = blogId;
+	this.textId = textId;
+}
+Blog.prototype = {
+	showContent : function() {
+		var _this = this;
+		this.blogId.click(function() {
+			var index = $(this).index();
+			for(var i = 0; i < _this.blogId.length; i++){
+				if(i == index){
+					_this.textId.eq(i).slideDown("slow");
+				}
+				else{
+					_this.textId.eq(i).slideUp();
+				}
+			}
+			return false;
+		});
+	}
+}
+$(document).ready(function(){
+	var blogData = new Blog($('#blog li'), $('.excerpt'));
+	blogData.showContent();
+});
+	
