4d6d385634b6aec972207d4b817c4a28229cd2ed
rachita12 <rachitanayyar23@gmail.com>
2016-07-18 10:33:09 +0530

    revised exeercise 4.1

--- jquery-exercise4.1/js/inputHint.js
+++ jquery-exercise4.1/js/inputHint.js
@@ -3,36 +3,36 @@ function SearchBox(targetOnEvent) {
   this.targetOnEvent = targetOnEvent;
 }
 SearchBox.prototype = {
-  
-	/* part A = Set the value of the search input to the text of the label element*/
+
+  /* part A = Set the value of the search input to the text of the label element*/
   changeLabel : function() {
     var _this = this;
     _this.targetOnEvent.keyup(function(){
       ($('label')).html($('.input_text').val());
     });
   },
-  
+
   /* part B = add a class "hint" to search input*/
   addClassToSearch: function() {
     this.targetOnEvent.addClass("hint");
   },
-  
+
   /*part C = Remove the label element*/
   removeLabel : function() {
     this.targetOnEvent.remove();
   },
-	
+
   /*part D = Bind a focus event to the search input that removes the hint text and the "hint" class*/
   onFocusEvent : function() {
-		var _this = this;
+    var _this = this;
     this.targetOnEvent.focus(function(){
       _this.targetOnEvent.val("").removeClass("hint");
     });
   },
-  
+
   /*part E = Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered*/
   onBlurEvent : function(){
-		var _this = this;
+    var _this = this;
     this.targetOnEvent.blur(function(){
       var searchValue = _this.targetOnEvent.val();
       if(searchValue == null || searchValue == ""){
