00343d2578da661e68aa2f921a710539efccf6bd
rachita12 <rachitanayyar23@gmail.com>
2016-07-23 17:37:31 +0530

    revised exercise using Delegate

--- addItems-usingDelegate/js/addItem.js
+++ addItems-usingDelegate/js/addItem.js
@@ -1,35 +1,35 @@
 function Items(btnId,containerDiv) {
-	"use strict"
-	this.btnId = btnId;
-	this.containerDiv = containerDiv;
-	this.count = 0;
+  "use strict"
+  this.btnId = btnId;
+  this.containerDiv = containerDiv;
+  this.count = 0;
 }
 Items.prototype = {
-	addDiv : function() {
-		var _this = this;
-		this.btnId.click(function(){
-			_this.containerDiv.append("<div>"+ _this.count + "</div>");
-			_this.count++;
-		});
-	},
-	highlightDiv : function() {
-		var _this = this;
-		this.containerDiv.delegate("div","click",function() {
-			var __this = $(this);
-			__this.addClass("itemDiv");
-			var index = __this.index();
-			console.log(index);
-			console.log(_this.count);
-			if(index == _this.count-1){
-				__this.remove();
-				_this.count--;
-			}
-		});
-	},
+  addDiv : function() {
+    var _this = this;
+    this.btnId.click(function(){
+      _this.containerDiv.append("<div>"+ _this.count + "</div>");
+      _this.count++;
+    });
+  },
+  highlightDiv : function() {
+  var _this = this;
+    this.containerDiv.delegate("div","click",function() {
+      var __this = $(this);
+      __this.addClass("itemDiv");
+      var index = __this.index();
+      if(index == _this.count-1){
+        __this.remove();
+        _this.count--;
+      }
+    });
+  },
+  bindEvents : function() {
+    this.addDiv();
+    this.highlightDiv();
+  }
 }
 $(document).ready(function(){
-	var newItem = new Items($('.btn'),$('.container'));
-	newItem.addDiv();
-	newItem.highlightDiv();
-	
+  var newItem = new Items($('.btn'),$('.container'));
+  newItem.bindEvents();
 });
